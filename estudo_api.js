ca6778cf9bf4f05193756cd2ce26b3ccd2a6d66a
Leonardo Ramos <leo.ehrhardt82@gmail.com>
2026-08-13 19:02:21 -0300

    Revert "travado"
    This reverts commit 9c24d073018acb63e62c642712d792b5daa7e592.

--- estudo_api.js
+++ estudo_api.js
@@ -5,3 +5,46 @@ const port = 3000;
 
 app.use(express.json());
 
+<<<<<<< HEAD
+=======
+let users = [
+  { id: 1, name: "John Doe", email: "john.doe@example.com" },
+  { id: 2, name: "Jane Smith", email: "jane.smith@example.com" },
+  { id: 3, name: "Bob Johnson", email: "bob.johnson@example.com" }
+];
+
+//listando
+app.get("/users", (req, res) => {
+  res.status(200).json(users);
+});
+
+
+//adicionando
+app.post("/users", (req, res) => {
+    const novoUsuario = req.body;
+
+    users.push(novoUsuario);
+
+    res.status(201).json({mensagem: "Usuário adicionado com sucesso!", usuario: novoUsuario});
+
+});
+
+//removendo
+app.delete("/users/:id", (req, res) => {
+    const id = Number(req.params.id);
+
+    users = users.filter(user => user.id !== id);
+
+    res.status(200).json({mensagem: "Usuário deletado com sucesso!"});
+});
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+>>>>>>> parent of 9c24d07 (travado)
