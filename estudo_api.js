8864f93192b1dd53aadb878bdd8a0b174d2fcffe
Leonardo Ramos <leo.ehrhardt82@gmail.com>
2026-08-15 10:45:18 -0300

    conectando mongoose

--- estudo_api.js
+++ estudo_api.js
@@ -1,11 +1,26 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
-
 app.use(express.json());
 
+const connectDB =async () => {
+    try{
+        await mongoose.connect(process.env.MONGO_URI,);
+        console.log("MongoDB conectado");
+    } catch (error) {
+        console.log("Erro ao conectar ao MongoDB:", error);
+    }
+};
+connectDB();
 
+app.post("/vendas")
+    
+    
 
 
 
